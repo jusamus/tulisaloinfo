b2e1d06f168e9ccbe10a65105d2bcc8d2244b17f
Jussi Tulisalo <jussi@tulisalo.info>
2020-01-11 13:50:09 +0200

    Start coding "Minesweeper" card game

---
+++ application/views/minesweeper.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<style type="text/css">
+    
+</style>
+<center>
+<div id="minesweeper">
+<br/>
+    <h4>Miinaharava</h4>
+    
+<script type="text/javascript">
+    $(document).ready(function(){
+    
+    });
+</script>
+</div>
+</center>
